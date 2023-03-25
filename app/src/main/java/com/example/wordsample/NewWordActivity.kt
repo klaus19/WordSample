4da03b26ec163460b85c465986125c4b437a4671
klaus19 <tejaskhartude@gmail.com>
2023-03-25 18:15:34 +0530

    added new button that will save and pass the data

--- app/src/main/java/com/example/wordsample/NewWordActivity.kt
+++ app/src/main/java/com/example/wordsample/NewWordActivity.kt
@@ -1,5 +1,6 @@
 package com.example.wordsample
 
+import android.annotation.SuppressLint
 import android.app.Activity
 import android.content.Intent
 import android.os.Bundle
@@ -11,15 +12,16 @@ import androidx.appcompat.app.AppCompatActivity
 class NewWordActivity:AppCompatActivity() {
 
     private lateinit var editWordView: EditText
+    val replyIntent = Intent()
 
     public override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.app_new_word)
         editWordView = findViewById(R.id.edit_word)
 
-        val button = findViewById<Button>(R.id.button_save)
-        button.setOnClickListener {
-            val replyIntent = Intent()
+        val btnSave = findViewById<Button>(R.id.button_save)
+        val btnGo = findViewById<Button>(R.id.buttonGo)
+        btnSave.setOnClickListener {
             if (TextUtils.isEmpty(editWordView.text)) {
                 setResult(Activity.RESULT_CANCELED, replyIntent)
             } else {
@@ -27,8 +29,14 @@ class NewWordActivity:AppCompatActivity() {
                 replyIntent.putExtra(EXTRA_REPLY, word)
                 setResult(Activity.RESULT_OK, replyIntent)
             }
+        }
+
+        btnGo.setOnClickListener {
+            setResult(Activity.RESULT_OK,replyIntent)
             finish()
         }
+
+
     }
 
     companion object {
